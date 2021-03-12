4ed9e52c7dd246046dd19311226e1aa378cabaf4
LEEJUNB <topguide82@gmail.com>
2021-03-12 09:48:24 +0900

    DB init

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model) :
+    title = models.CharField(max_length=200,default = '') # 문자열, 괄호안은 제약사항
+    body = models.TextField() # 대용량 문자열
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) : 
+        return self.title
