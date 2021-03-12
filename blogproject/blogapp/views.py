17b4f57744ea4e6811d65ffcc404eaa19aeb0db0
LEEJUNB <topguide82@gmail.com>
2021-03-12 10:31:02 +0900

    게시글을 DB에 반영하는 함수

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect # redirect : 함수 실행시 특정 html,url로 이동하도록함  
+from .models import Blog
+from django.utils import timezone
 
 def home(request) :
     return render(request, 'index.html')
@@ -7,6 +9,14 @@ def home(request) :
 def new(request) :
     return render(request, 'new.html')
 
-# 블로그 글 저장 함수
+# 블로그 글 저장 함수. 작성한 글이 DB에 반영
+# new.html에서 버튼을 눌렀을 때 실행되는 함수
+# 렌더링을 해서 특정 html을 보여주는 함수가 아님
 def create(request) :
-    return render(request, 'create.html')
+    if(request.method == "POST") :
+        post = Blog() # post변수에 Blog객체생성
+        post.title = request.POST['title'] # title에 해당되는 것을 post.title에 담음
+        post.body = request.POST['body']
+        post.date = timezone.now()
+        post.save() # model객체.save()를 통해 모델 객체를 DB에 저장
+    return redirect('home')
