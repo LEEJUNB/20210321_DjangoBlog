541b90847245918069eb76b7c9fec066d78a1c4d
LEEJUNB <topguide82@gmail.com>
2021-03-12 10:18:18 +0900

    DB : HTML Form

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.s
 def home(request) :
     return render(request, 'index.html')
+
+# 블로그 글 작성 html
+def new(request) :
+    return render(request, 'new.html')
+
+# 블로그 글 저장 함수
+def create(request) :
+    return render(request, 'create.html')
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -5,4 +5,8 @@ from blogapp import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+
+    # html form 이용한 블로그 객체
+    path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'),
 ]
