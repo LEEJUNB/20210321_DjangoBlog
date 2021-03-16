b4ae5fa7c119a72929dca53516728feca11eae96
LEEJUNB <topguide82@gmail.com>
2021-03-16 11:29:10 +0900

    comment function 1

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -9,3 +9,18 @@ class Blog(models.Model) :
     # title이 DB 목록에 표현
     def __str__(self) : 
         return self.title
+
+# Blog 객체에 종속적인 comment 객체
+class Comment(models.Model) :
+    comment = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    # 게시글에 댓글이 달리게 만드는 post 변수
+    # 게시글 작성 역할인 Blog 객체에 종속. 
+    # post 변수는 Blog 객체를 참조하는 칼럼 (참조키,ForeignKey)
+    # 게시글(Blog 객체) 삭제시 댓글(comment 객체)도 삭제
+    post = models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    # 댓글작성시 object(num)이 아닌 작성한 댓글 자체가 나타나도록
+    def __str__(self) : 
+        return self.comment
